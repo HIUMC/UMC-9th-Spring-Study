8c38ebded9b165facad44724e326ebf6cf2d7f92
minjeong <litt22@g.hongik.ac.kr>
2025-09-24 00:22:36 +0900

    스프링 입문

---
+++ littby/스프링입문/hello-spring/src/main/java/hello/hello_spring/controller/MemberController.java
@@ -0,0 +1,44 @@
+package hello.hello_spring.controller;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
+
+@Controller
+public class MemberController {
+
+    //하나만 등록해서 쓰기
+    private final MemberService memberService;
+
+    @Autowired //연결 시켜줄 때 오토와이어 쓰기
+    public MemberController(MemberService memberService) {
+        this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping(value = "/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+        memberService.join(member);
+        return "redirect:/";
+    }
+
+    //어떤 주소랑 매칭될거냥
+    @GetMapping(value = "/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
+}
---
+++ littby/스프링입문/hello-spring/src/test/java/hello/hello_spring/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,57 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional //테스트 끝나면 롤백을 해줌 : 테스트를 계속 할수있땅
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+    @Test
+    void 회원가입() {
+        //given 머가 주어졌는데 (어떤 데이터를 기반으로)
+        Member member = new Member();
+        member.setName("spring");
+
+        //when 이걸 실행했을 때
+        Long savedId = memberService.join(member);
+
+        //then 결과가 이게 나와야됨
+        Member findMember = memberService.findOne(savedId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //then
+
+    }
+
+
+}
