d1cb78059c774c54246166cf1e8c503abb641528
seungju <lsjxcs@gmail.com>
2025-09-24 19:52:21 +0900

    [Feat(이승주(uno))] : 스프링 입문 추가

---
+++ 스프링 입문/hello-spring/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,45 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+public class MemberService {
+    /**
+     * 회원가입
+     */
+    public Long join(Member member) {
+        long start = System.currentTimeMillis();
+
+        try {
+            validateDuplicateMember(member); //중복 회원 검증
+            memberRepository.save(member);
+            return member.getId();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("join " + timeMs + "ms");
+        }
+    }
+
+    /**
+     * 전체 회원 조회
+     */
+    public List<Member> findMembers() {
+        long start = System.currentTimeMillis();
+
+        try {
+            return memberRepository.findAll();
+        } finally {
+            long finish = System.currentTimeMillis();
+            long timeMs = finish - start;
+            System.out.println("findMembers " + timeMs + "ms");
+        }
+    }
+}
+
