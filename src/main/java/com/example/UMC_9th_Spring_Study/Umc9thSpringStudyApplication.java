f3be6c800ac7d20170fb74b3f76f6993aa23f7a1
seungju <lsjxcs@gmail.com>
2025-10-01 00:16:38 +0900

    [fix] gitignore 수정3

---
+++ src/main/java/com/example/UMC_9th_Spring_Study/Umc9thSpringStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.UMC_9th_Spring_Study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Umc9thSpringStudyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Umc9thSpringStudyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/UMC_9th_Spring_Study/Umc9thSpringStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UMC_9th_Spring_Study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Umc9thSpringStudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
