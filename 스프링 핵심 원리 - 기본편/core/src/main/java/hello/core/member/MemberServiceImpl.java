9d3915749f9ddc55b2251bb30bab48e8667fad93
GitHub 핸들명 <GitHub >
2025-10-01 00:12:37 +0900

    2주차 - 스프링 핵심원리 기본편

--- 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
+++ 스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService {
 
     // NullPointerException - 구현 객체 선택해주기
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
