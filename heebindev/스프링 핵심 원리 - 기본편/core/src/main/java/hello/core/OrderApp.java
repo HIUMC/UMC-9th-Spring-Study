6826bd081a761bd5dc7a6da64b959a3a2a61b767
히빈스 맥북 <heebin_mac@hibinseuui-MacBookPro.local>
2025-09-30 16:05:28 +0900

    섹션 3까지 코드 실습완료

---
+++ heebindev/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/OrderApp.java
@@ -0,0 +1,26 @@
+package hello.core;
+
+import hello.core.member.Grade;
+import hello.core.member.Member;
+import hello.core.member.MemberService;
+import hello.core.member.MemberServiceImpl;
+import hello.core.order.Order;
+import hello.core.order.OrderService;
+import hello.core.order.OrderServiceImpl;
+
+public class OrderApp {
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        OrderService orderService = new OrderServiceImpl();
+
+        Long memberId = 1l;
+        Member member = new Member(memberId, "memberA", Grade.VIP);//member만들고
+        memberService.join(member);//그 member를 회원가입시키기.
+
+        Order order = orderService.createOrder(memberId,"itemA",10000);
+        //order생성하기
+
+        System.out.println("order = " + order);//인자 order는 order.toString()으로 호출
+        System.out.println("order = " + order.calculatePrice());
+    }
+}
