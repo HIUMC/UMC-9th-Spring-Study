bb992b6e71a3c2704a8f9e76ff451f468fc5d69d
GitHub 핸들명 <GitHub >
2025-10-01 12:28:00 +0900

    [Chore(데이/이은채) : 디렉토리 구조 변경

---
+++ euuunchae/스프링 핵심 원리 - 기본편/core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,22 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService {
+
+    // NullPointerException - 구현 객체 선택해주기
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+}
