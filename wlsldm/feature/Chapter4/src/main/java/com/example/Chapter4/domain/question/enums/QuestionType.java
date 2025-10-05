5d6192e4fa301ecc286e4271c32173538ba2095e
조수진 <lavesujin@gmail.com>
2025-10-05 21:24:28 +0900

    4주차 미션

---
+++ wlsldm/feature/Chapter4/src/main/java/com/example/Chapter4/domain/question/enums/QuestionType.java
@@ -0,0 +1,5 @@
+package com.example.Chapter4.domain.question.enums;
+
+public enum QuestionType {
+    MISSION, STORE, REVIEW, POINT, ETC
+}
---
+++ wlsldm/feature/Chapter4/src/main/java/com/example/Chapter4/domain/review/entity/ReviewReply.java
@@ -0,0 +1,29 @@
+package com.example.Chapter4.domain.review.entity;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+
+@Builder
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "reviewReplies")
+@EntityListeners(AuditingEntityListener.class)
+public class ReviewReply {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "review_id")
+    private Review review;
+
+    @Column(name = "context", nullable = false)
+    private String context;
+}
