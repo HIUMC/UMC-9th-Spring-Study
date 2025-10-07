bad7e02210abfef5a3d154e94b0cc508b5ac6842
jeun66 <sje040626@naver.com>
2025-10-07 12:23:05 +0900

    스프링 핵심 원리-기본편 5,6,7

--- jeun66/core/src/main/java/hello/core/AppConfig.java
+++ jeun66/core/src/main/java/hello/core/AppConfig.java
@@ -1,4 +1,5 @@
-package hello.core.order;
+package hello.core;
+
 import hello.core.discount.DiscountPolicy;
 import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.MemberRepository;
@@ -9,20 +10,26 @@ import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 @Configuration
 public class AppConfig {
     @Bean
     public MemberService memberService() {
+        //1번
+        System.out.println("call AppConfig.memberService");
         return new MemberServiceImpl(memberRepository());
     }
     @Bean
     public OrderService orderService() {
+        //1번
+        System.out.println("call AppConfig.orderService");
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
     @Bean
     public MemberRepository memberRepository() {
+        System.out.println("call AppConfig.memberRepository");
         return new MemoryMemberRepository();
     }
     @Bean
--- jeun66/core/src/test/java/hello/core/order/OrderServiceTest.java
+++ jeun66/core/src/test/java/hello/core/order/OrderServiceTest.java
@@ -1,11 +1,7 @@
 package hello.core.order;
-import hello.core.member.Grade;
-import hello.core.member.Member;
+import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.*;
+
 class OrderServiceTest {
     MemberService memberService;
     OrderService orderService;
