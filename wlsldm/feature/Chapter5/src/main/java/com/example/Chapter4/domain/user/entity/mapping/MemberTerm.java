f00fa460b35f799d1a0bc86cb481d3047455eace
조수진 <lavesujin@gmail.com>
2025-10-26 18:27:47 +0900

    5주차  폴더 생성

---
+++ wlsldm/feature/Chapter5/src/main/java/com/example/Chapter4/domain/user/entity/mapping/MemberTerm.java
@@ -0,0 +1,29 @@
+package com.example.Chapter4.domain.user.entity.mapping;
+
+import com.example.Chapter4.domain.user.entity.Member;
+import com.example.Chapter4.domain.user.entity.Term;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Table(name = "member_term")
+public class MemberTerm {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "term_id")
+    private Term term;
+
+}
