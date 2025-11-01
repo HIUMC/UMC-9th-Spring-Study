7f9f40266b2c24e72dada4dd0172a59f8703bd06
junjunseo <jun85862@gmail.com>
2025-11-02 02:56:35 +0900

    워크북 6주차 실습 및 과제 제출

--- junjunseo/워크북실습/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
+++ junjunseo/워크북실습/src/main/java/com/example/umc9th/domain/review/repository/ReviewRepository.java
@@ -5,9 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 
-public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+public interface ReviewRepository extends JpaRepository<Review, Long>, ReviewQueryDsl {
 
     //내가 작성한 리뷰 (최신순, 페이징)
     Page<Review> findByMemberIdOrderByCreatedAtDesc(Long memberId, Pageable pageable);
+
 }
