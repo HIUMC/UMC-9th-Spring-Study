3d9d209b7567bf74f5f35c902d62cbfb8122b01c
GitHub 핸들명 <GitHub >
2025-11-13 09:07:05 +0900

    8주차

---
+++ euuunchae/스프링 부트와 JPA 활용1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,41 @@
+package jpabook.jpashop.repository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MemberRepository {
+
+
+    private final EntityManager em;
+
+    // 저장
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    // 조회
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    // 전체 조회
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();  // 1.JPQL, 2.반환타입
+
+    }
+
+    // 이름으로 조회
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+}
