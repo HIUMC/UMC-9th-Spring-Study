89193ffdccb8cc36c3972716cc66e2e9b16ee601
spacejin2000 <spacejin2000@naver.com>
2025-11-13 12:36:57 +0900

    스프링 JPA 활용 파트 3, 4, 5, 6 완료

---
+++ BestTomaTo/스프링 스터디 워크북/demo/demo/src/main/generated/HongikSpring/demo/review/QReview.java
@@ -0,0 +1,41 @@
+package HongikSpring.demo.review;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QReview is a Querydsl query type for Review
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QReview extends EntityPathBase<Review> {
+
+    private static final long serialVersionUID = 510058338L;
+
+    public static final QReview review = new QReview("review");
+
+    public final StringPath body = createString("body");
+
+    public final NumberPath<Long> review_id = createNumber("review_id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public QReview(String variable) {
+        super(Review.class, forVariable(variable));
+    }
+
+    public QReview(Path<? extends Review> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QReview(PathMetadata metadata) {
+        super(Review.class, metadata);
+    }
+
+}
+
