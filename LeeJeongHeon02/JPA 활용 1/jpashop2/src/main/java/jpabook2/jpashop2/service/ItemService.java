a8cb48616f481d7af3fe05a4d0dbaa0166f01d5e
LeeJeongHeon02 <aoc05230@naver.com>
2025-11-20 14:07:28 +0900

    All FInish

--- LeeJeongHeon02/JPA 활용 1/jpashop2/src/main/java/jpabook2/jpashop2/service/ItemService.java
+++ LeeJeongHeon02/JPA 활용 1/jpashop2/src/main/java/jpabook2/jpashop2/service/ItemService.java
@@ -1,7 +1,9 @@
 package jpabook2.jpashop2.service;
 
+import jpabook2.jpashop2.domain.item.Book;
 import jpabook2.jpashop2.domain.item.Item;
 import jpabook2.jpashop2.repository.ItemRepository;
+import jpabook2.jpashop2.web.BookForm;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,10 +21,21 @@ public class ItemService {
         itemRepository.save(item);
     }
 
+    public void updateItem(Long itemId, BookForm form) {
+        Book book = (Book) itemRepository.findOne(itemId);
+        book.setName(form.getName());
+        book.setPrice(form.getPrice());
+        book.setStockQuantity(form.getStockQuantity());
+        book.setAuthor(form.getAuthor());
+        book.setIsbn(form.getIsbn());
+    }
+
+    @Transactional(readOnly = true)
     public List<Item> findItems() {
         return itemRepository.findAll();
     }
 
+    @Transactional(readOnly = true)
     public Item findOne(Long id) {
         return itemRepository.findOne(id);
     }
