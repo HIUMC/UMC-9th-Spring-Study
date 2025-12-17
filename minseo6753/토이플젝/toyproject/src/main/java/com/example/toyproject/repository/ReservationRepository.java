2f1a765453bdba9245a649c8a7a99bac39489c59
minseo6753 <minseo6753@naver.com>
2025-12-18 05:15:53 +0900

    fix(토이플젝): id 타입 Long

--- minseo6753/토이플젝/toyproject/src/main/java/com/example/toyproject/repository/ReservationRepository.java
+++ minseo6753/토이플젝/toyproject/src/main/java/com/example/toyproject/repository/ReservationRepository.java
@@ -3,5 +3,5 @@ package com.example.toyproject.repository;
 import com.example.toyproject.domain.Reservation;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ReservationRepository extends JpaRepository<Reservation,Integer> {
+public interface ReservationRepository extends JpaRepository<Reservation,Long> {
 }
